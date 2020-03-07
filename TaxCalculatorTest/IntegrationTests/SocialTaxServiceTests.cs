65d8d499ecc5181f9732a781bf7ec29a782c58dc
Mihail Katsarov <mihail.katsarow@gmail.com>
2020-03-07 13:36:51 +0200

    Tests added

---
+++ TaxCalculatorTest/IntegrationTests/SocialTaxServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TC.Services;
+
+namespace TaxCalculatorTest.IntegrationTests
+{
+    [TestClass]
+    public class SocialTaxServiceTests
+    {
+        [TestMethod]
+        public void CalculateTax_ShouldReturn_Correct()
+        {
+            var grossSalary = 3400m;
+            var salaryFreeOfTax = 1000m;
+            var expectedResult = 300m;
+
+            var socialService = new SocialTaxService();
+
+            var result = socialService.CalculateTax(grossSalary, salaryFreeOfTax);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ TaxCalculatorTest/UnitTests/ITaxCalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TC.Services.Contracts;
+
+namespace TaxCalculatorTest.UnitTests
+{
+    [TestClass]
+    public class ITaxCalculatorTests
+    {
+        private decimal salaryFreeOfTax = 1000m;
+        private decimal grossSalary = 1200m;
+
+        //Verifing all Calculate methods where T : ITaxCalculator 
+        [TestMethod]
+        public void Calculate_ShouldBeInvokeOnce()
+        {
+            var incomeSerivceMock = new Mock<ITaxCalculator>();
+            incomeSerivceMock.Setup(c => c.CalculateTax(grossSalary, salaryFreeOfTax)).Returns(20m);
+
+            incomeSerivceMock.Object.CalculateTax(grossSalary, salaryFreeOfTax);
+
+            incomeSerivceMock.Verify(c => c.CalculateTax(grossSalary, salaryFreeOfTax), Times.Once);
+        }
+    }
+}
